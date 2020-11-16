4ff529130b332e3fa838222336be980e2e169294
chenuvi <chenuvi@outlook.com>
2020-11-16 16:11:15 +0800

    Feat: 使用 router-link

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <Chenuvi />
+  <div>导航栏 |
+  <router-link to="/">1</router-link></div> |
+  <router-link to="/xxx">2</router-link></div>
+  <hr />
+  <router-view />
+
 </template>
 
 <script>
@@ -7,8 +12,5 @@ import Chenuvi from './components/Chenuvi.vue'
 
 export default {
   name: 'App',
-  components: {
-    Chenuvi
-  }
 }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,17 @@ import App from './App.vue'
 import './index.css'
 import { createWebHashHistory, createRouter } from 'vue-router'
 import Chenuvi from './components/Chenuvi.vue'
+import Chenuvi2 from './components/Chenuvi2.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
-    history:history,
-    routes:[
-        {path:'/',component:Chenuvi}
+    history: history,
+    routes: [
+        { path: '/', component: Chenuvi },
+        { path: '/xxx', component: Chenuvi2 }
     ]
 })
 
-const app = createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
