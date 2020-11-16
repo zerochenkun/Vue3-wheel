a8a14ecb52a2b8549a4b515e2c108d3e49875e46
chenuvi <chenuvi@outlook.com>
2020-11-16 14:32:23 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Chenuvi />
+</template>
+
+<script>
+import Chenuvi from './components/Chenuvi.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Chenuvi
+  }
+}
+</script>
