fc9778cadc7fd7305c1d1dd32d5b9aef2aec6e9c
chenuvi <chenuvi@outlook.com>
2020-11-17 14:53:37 +0800

    Css: change to index.scss

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+import './index.scss'
 import { createWebHashHistory, createRouter } from 'vue-router'
 import Home from './pages/Home.vue'
 import Doc from './pages/Doc.vue'
