0c7055ef8a990306c76af05fbaeeba2a032c32cd
chenuvi <chenuvi@outlook.com>
2020-11-17 15:54:54 +0800

    Feat: aside's HTML and css

--- src/pages/Doc.vue
+++ src/pages/Doc.vue
@@ -1,7 +1,23 @@
 <template>
- <Topnav />
+  <Topnav />
   <div class="content">
-    <aside>边栏</aside>
+    <aside>
+      <h2>组件列表</h2>
+      <ol>
+        <li>
+          <router-link to="/doc/swich">Switch 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/button">Button 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/dialog">Dialog 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/tabs">Tabs 组件</router-link>
+        </li>
+      </ol>
+    </aside>
     <main>主内容</main>
   </div>
 </template>
@@ -9,5 +25,21 @@
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
 
-export default { components: {Topnav} };
+export default { components: { Topnav } };
 </script>
+
+<style lang="scss">
+  aside{
+    border:1px solid red;
+    width:150px;
+    padding:16px;
+    > h2{
+      margin-bottom:4px;
+    }
+    > ol {
+      > li {
+        padding:4px 0;
+      }
+    }
+  }
+</style>
