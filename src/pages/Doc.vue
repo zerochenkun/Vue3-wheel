9009fb98f71c24ee85207237d0712ff91ce3f6ff
chenuvi <chenuvi@outlook.com>
2020-11-21 22:59:33 +0800

    Css： Doc样式

--- src/pages/Doc.vue
+++ src/pages/Doc.vue
@@ -1,26 +1,28 @@
 <template>
-  <Topnav />
-  <div class="content">
-    <aside v-if="asideVisiable">
-      <h2>组件列表</h2>
-      <ol>
-        <li>
-          <router-link to="/doc/switch">Switch 组件</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/button">Button 组件</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/dialog">Dialog 组件</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/tabs">Tabs 组件</router-link>
-        </li>
-      </ol>
-    </aside>
-    <main>
-      <router-view />
-    </main>
+  <div class="layout">
+    <Topnav class="nav" />
+    <div class="content">
+      <aside v-if="asideVisiable">
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs 组件</router-link>
+          </li>
+        </ol>
+      </aside>
+      <main>
+        <router-view />
+      </main>
+    </div>
   </div>
 </template>
 
@@ -39,23 +41,42 @@ export default {
 </script>
 
 <style lang="scss">
-aside {
+.layout {
   border: 1px solid red;
-  width: 150px;
-  padding: 16px;
-  > h2 {
-    margin-bottom: 4px;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav {
+    flex-shrink: 0;
   }
-  > ol {
-    > li {
-      padding: 4px 0;
+  > .content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    @media (max-width: 500px) {
+      padding-left: 0;
     }
   }
-  @media (max-width: 500px) {
+  aside {
+    background: lightblue;
+    width: 150px;
+    padding: 16px;
     position: fixed;
     top: 0;
     left: 0;
     padding-top: 70px;
+    height: 100%;
+    > h2 {
+      margin-bottom: 4px;
+    }
+    > ol {
+      > li {
+        padding: 4px 0;
+      }
+    }
+  }
+  main {
+    overflow: auto;
   }
 }
 </style>
