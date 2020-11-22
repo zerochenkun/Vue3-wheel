51bd58985e300c193b521a869a3c0c5051f6a70c
chenuvi <chenuvi@outlook.com>
2020-11-22 11:24:08 +0800

    Css: hover 之后右滑

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-    <p>Switch 组件</p>
-  </div>
+  <button><span></span></button>
 </template>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  border-radius: $h/2;
+  background: blue;
+  position: relative;
+  &:hover > span {
+    left: calc(100% - #{$h2} - 2px);
+  }
+}
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  border-radius: $h2 / 2;
+  background: white;
+}
+</style>
