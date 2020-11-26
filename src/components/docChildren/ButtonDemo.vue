2d925067552f210f675e43222d12147f95396871
chenuvi <chenuvi@outlook.com>
2020-11-26 17:40:13 +0800

    Feat: 创建 Button 组件

--- src/components/docChildren/ButtonDemo.vue
+++ src/components/docChildren/ButtonDemo.vue
@@ -1,3 +1,12 @@
 <template>
-    <div>ButtonDemo 的文档</div>
+  <div>ButtonDemo 示例</div>
+  <h1>示例1</h1>
+  <Button>你好</Button>
 </template>
+
+<script>
+import Button from "../../lib/Button.vue";
+export default {
+  components: { Button },
+};
+</script>
