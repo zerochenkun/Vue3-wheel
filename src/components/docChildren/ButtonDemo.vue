a46fcc520d91daea9f51804637a2fa2b2d2008ad
chenuvi <chenuvi@outlook.com>
2020-11-26 18:47:24 +0800

    Feat: 动态添加 theme class 属性

--- src/components/docChildren/ButtonDemo.vue
+++ src/components/docChildren/ButtonDemo.vue
@@ -1,7 +1,10 @@
 <template>
   <div>ButtonDemo 示例</div>
   <h1>示例1</h1>
-  <Button @click="onClick" @focus="onClick" @mouseover="onClick" size="small" disabled mytest="if not in props,then show in attrs">你好</Button>
+  <Button >你好</Button>
+  <Button theme="button">你好</Button>
+  <Button theme="link">你好</Button>
+  <Button theme="text">你好</Button>
 </template>
 
 <script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,24 +1,17 @@
 <template>
-  <div :size="size">
-    <button v-bind="rest">
-      <slot />
-    </button>
-  </div>
+  <button class="gulu-button" :class="{ [`theme-${theme}`]: theme }">
+    <slot />
+  </button>
 </template>
 
 <script lang="ts">
 export default {
   inheritAttrs: false,
   props: {
-    size: String,
-    disabled: Boolean,
-  },
-  setup(props, context) {
-    console.log("props: ", props);
-    const { size, ...rest } = context.attrs;
-    console.log("context.attrs: ", { ...context.attrs });
-    // const {size , onClick , onMouseover} = context.attrs
-    return { size, rest };
+    theme: {
+      type: String,
+      default: "button",
+    },
   },
 };
 </script>
